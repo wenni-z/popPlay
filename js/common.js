6db6400d1b63644f10fb6b7a70417c3880bbdc74
wenni-z <77379561+wenni-z@users.noreply.github.com>
2023-05-11 20:31:09 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -21,7 +21,7 @@ function getVue() {
 }
 
 function aaa() {
-    window.location.href = "http://127.0.0.1:5500/index.html";
+    window.location.href = "https://wenni-z.github.io/videovip/";
 }
 
 // $(document).ready(function () {
